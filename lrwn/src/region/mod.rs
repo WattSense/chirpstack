cb6515452dcd4393f54bd14543807ab3a63fa997
Mohamed Belaouad <mohamed.belaouad@wattsense.com>
2023-09-07 16:40:35 +0200

    Use FromSql for *const str instead of String
    This is to avoid error below when multiple backends are enabled within
    diesel. There are 2 options, switch the generig *const str to String or
    change the from_sql to use String. The first was chosen since it also
    avoids some extra allocation.
    
    cannot infer type for type parameter `DB` declared on the trait `FromSql`

--- lrwn/src/region/mod.rs
+++ lrwn/src/region/mod.rs
@@ -64,8 +64,8 @@ where
     *const str: deserialize::FromSql<Text, DB>,
 {
     fn from_sql(value: <DB as Backend>::RawValue<'_>) -> deserialize::Result<Self> {
-        let string = String::from_sql(value)?;
-        Ok(CommonName::from_str(&string)?)
+        let string = <*const str>::from_sql(value)?;
+        Ok(Self::from_str(unsafe { &*string })?)
     }
 }
 
@@ -175,8 +175,8 @@ where
     *const str: deserialize::FromSql<Text, DB>,
 {
     fn from_sql(value: <DB as Backend>::RawValue<'_>) -> deserialize::Result<Self> {
-        let string = String::from_sql(value)?;
-        Ok(Revision::from_str(&string)?)
+        let string = <*const str>::from_sql(value)?;
+        Ok(Self::from_str(unsafe { &*string })?)
     }
 }
 
@@ -260,8 +260,8 @@ where
     *const str: deserialize::FromSql<Text, DB>,
 {
     fn from_sql(value: <DB as Backend>::RawValue<'_>) -> deserialize::Result<Self> {
-        let string = String::from_sql(value)?;
-        Ok(MacVersion::from_str(&string)?)
+        let string = <*const str>::from_sql(value)?;
+        Ok(Self::from_str(unsafe { &*string })?)
     }
 }
 
